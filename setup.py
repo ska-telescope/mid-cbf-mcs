34c688cd1e603158dc0c2eba069497973e2f8721
Michelle Radulescu <michelle.radulescu@mdacorporation.com>
2021-07-06 23:15:07 -0700

    AT5-735 Correct url

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     long_description=readme + '\n\n',
     author="Taylor Huang",
     author_email='taylor.huang@mda.space',
-    url='https://gitlab.com/ska-telescope/ska-mid-cbf-mcs',
+    url='https://gitlab.com/ska-telescope/mid-cbf-mcs',
     packages=[
         'CbfMaster',
 	    'CbfSubarray'
