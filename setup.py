2a395ce6ef5e21f9844576e38cb2f6a97f58ced2
taylorhuang <taylor.huang@mda.space>
2021-07-20 11:42:56 -0700

    bump version number for repo rename

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ pack = ['ska-mid-cbf-mcs']
 
 setup(
     name='ska-mid-cbf-mcs',
-    version='0.5.3',
+    version='0.5.4',
     description="SKA Mid.CBF MCS",
     long_description=readme + '\n\n',
     author="Taylor Huang",
