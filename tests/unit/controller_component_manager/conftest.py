7703c7761770bccd90ca79e772150259a12a0240
Amanda Punch <amanda.punch@mda.space>
2022-01-21 11:02:50 -0800

    AT5-814 refactor controller component manager unit tests

--- tests/unit/controller_component_manager/conftest.py
+++ tests/unit/controller_component_manager/conftest.py
@@ -13,83 +13,112 @@ from __future__ import annotations
 import logging
 import pytest
 import unittest
-from typing import Any, Dict
+from typing import Dict, Callable
 
 import tango
 
 import os
+
+from ska_mid_cbf_mcs.component.component_manager import CommunicationStatus
 file_path = os.path.dirname(os.path.abspath(__file__))
 import json
+import functools
 
 # Local imports
 
-# uncomment the following after update to version 0.11.3 of base classes
-# from ska_mid_cbf_mcs.controller.controller_component_manager import ControllerComponentManager 
-from ska_tango_base.control_model import SimulationMode
+from ska_mid_cbf_mcs.controller.controller_component_manager import ControllerComponentManager 
+from ska_tango_base.control_model import PowerMode, SimulationMode
 from ska_mid_cbf_mcs.testing.mock.mock_device import MockDeviceBuilder
 from ska_tango_base.commands import ResultCode
 from ska_tango_base.control_model import HealthState, AdminMode, ObsState
 from ska_mid_cbf_mcs.testing.mock.mock_device import MockDeviceBuilder
 from ska_mid_cbf_mcs.testing.mock.mock_group import MockGroupBuilder
 from ska_mid_cbf_mcs.testing.tango_harness import TangoHarness
+from ska_mid_cbf_mcs.testing.mock.mock_callable import MockChangeEventCallback
 
-# uncomment the following after update to version 0.11.3 of base classes
-# @pytest.fixture(scope="function")
-# def controller_component_manager(
-#     logger: logging.Logger,
-#     tango_harness: TangoHarness # sets the connection_factory
-# ) -> ControllerComponentManager:
-#     """
-#     Return a Controller component manager.
+@pytest.fixture(scope="function")
+def controller_component_manager(
+    logger: logging.Logger,
+    tango_harness: TangoHarness # sets the connection_factory
+) -> ControllerComponentManager:
+    """
+    Return a Controller component manager.
 
-#     :param logger: the logger fixture
+    :param logger: the logger fixture
 
-#     :return: a Controller component manager.
-#     """
+    :return: a Controller component manager.
+    """
 
-#     class MockTalonDxComponentManager:
-#         """
-#         Class to mock the TalonDxComponentManager.
-#         """
-#         def __init__(self: MockTalonDxComponentManager) -> None:
-#             pass
+    class MockTalonDxComponentManager:
+        """
+        Class to mock the TalonDxComponentManager.
+        """
+        def __init__(self: MockTalonDxComponentManager) -> None:
+            pass
 
-#         def configure_talons(self: MockTalonDxComponentManager) -> ResultCode:
-#             return ResultCode.OK
+        def configure_talons(self: MockTalonDxComponentManager) -> ResultCode:
+            return ResultCode.OK
     
-#     f = open(file_path + "/../../data/controller_component_manager.json")
-#     json_string = f.read().replace("\n", "")
-#     f.close()
-#     configuration = json.loads(json_string)
+    f = open(file_path + "/../../data/controller_component_manager.json")
+    json_string = f.read().replace("\n", "")
+    f.close()
+    configuration = json.loads(json_string)
 
-#     fqdn_vcc = configuration["fqdn_vcc"]
-#     fqdn_fsp = configuration["fqdn_fsp"]
-#     fqdn_subarray = configuration["fqdn_subarray"]
-#     fqdn_talon_lru = configuration["fqdn_talon_lru"]
+    vcc = configuration["fqdn_vcc"]
+    fsp = configuration["fqdn_fsp"]
+    subarray = configuration["fqdn_subarray"]
+    talon_lru = configuration["fqdn_talon_lru"]
 
-#     count_vcc = configuration["count_vcc"]
-#     count_fsp = configuration["count_fsp"]
-#     count_subarray = configuration["count_subarray"]
-#     count_talon_lru = configuration["count_talon_lru"]
+    def mock_get_num_capabilities():
+        num_capbilities = {
+                            "VCC": 4,
+                            "FSP": 4,
+                            "Subarray": 1 
+                        }
+        return num_capbilities
+    
+    def mock_communication_status_changed_callback(communication_status: CommunicationStatus):
+        pass
 
-#     talondx_component_manager = MockTalonDxComponentManager()
+    def mock_component_power_mode_changed_callback(power_mode: PowerMode):
+        pass
 
-#     return ControllerComponentManager( 
-#             [
-#                 fqdn_vcc,
-#                 fqdn_fsp,
-#                 fqdn_subarray,
-#                 fqdn_talon_lru
-#             ],
-#             [
-#                 count_vcc, 
-#                 count_fsp,
-#                 count_subarray, 
-#                 count_talon_lru,
-#             ],
-#             talondx_component_manager,
-#             logger,
-#         )
+    talondx_component_manager = MockTalonDxComponentManager()
+
+    return ControllerComponentManager( 
+            mock_get_num_capabilities,
+            vcc,
+            fsp,
+            subarray,
+            talon_lru,
+            talondx_component_manager,
+            logger,
+            mock_change_event_callback_factory,
+            mock_communication_status_changed_callback,
+            mock_component_power_mode_changed_callback,
+        )
+
+@pytest.fixture()
+def mock_change_event_callback_factory(
+    mock_callback_called_timeout: float,
+    mock_callback_not_called_timeout: float,
+) -> Callable[[str], MockChangeEventCallback]:
+    """
+    Return a factory that returns a new mock change event callback each call.
+
+    :param mock_callback_called_timeout: the time to wait for a mock
+        callback to be called when a call is expected
+    :param mock_callback_not_called_timeout: the time to wait for a mock
+        callback to be called when a call is unexpected
+
+    :return: a factory that returns a new mock change event callback
+        each time it is called with the name of a device attribute.
+    """
+    return functools.partial(
+        MockChangeEventCallback,
+        called_timeout=mock_callback_called_timeout,
+        not_called_timeout=mock_callback_not_called_timeout,
+    )
 
 @pytest.fixture()
 def mock_vcc() -> unittest.mock.Mock:
