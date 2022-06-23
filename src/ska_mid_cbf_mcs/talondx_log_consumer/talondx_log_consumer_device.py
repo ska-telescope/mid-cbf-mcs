3e66a0eeb7126a85e327c713676a0c54a7affaea
taylorhuang <taylorw.huang@gmail.com>
2022-06-23 09:58:21 -0700

    CIP-907 remove unused import tango.server.attribute

--- src/ska_mid_cbf_mcs/talondx_log_consumer/talondx_log_consumer_device.py
+++ src/ska_mid_cbf_mcs/talondx_log_consumer/talondx_log_consumer_device.py
@@ -27,7 +27,7 @@ from ska_tango_base.base.base_device import (
 from ska_tango_base.commands import BaseCommand, ResultCode
 from ska_tango_base.control_model import LoggingLevel
 from ska_tango_base.faults import LoggingLevelError
-from tango.server import attribute, command, run
+from tango.server import command, run
 
 # PROTECTED REGION END #    //  TalonDxLogConsumer.additional_import
 
