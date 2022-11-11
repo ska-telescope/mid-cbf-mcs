a1596a0d3216633b91d9229e43eea9af37cec5a0
taylorhuang <taylor.huang@mda.ca>
2022-11-10 16:29:51 -0800

    CIP-848 add DISH ID str to int and int to str conversion methods

---
+++ src/ska_mid_cbf_mcs/commons/dish_id_utils.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SKA Mid.CBF MCS project
+#
+# Distributed under the terms of the GPL license.
+# See LICENSE for more info.
+
+"""This module implements utilities for managing DISH/receptor identifiers."""
+
+from __future__ import annotations  # allow forward references in type hints
+
+__all__ = ["receptor_id_str_to_int", "receptor_id_int_to_str"]
+
+
+def receptor_id_str_to_int(receptor_id: str) -> int:
+    """
+    Convert DISH/receptor ID mnemonic string to integer.
+
+    :param receptor_id: the DISH/receptor ID mnemonic as a string
+
+    :return: the DISH/receptor ID as a sequential integer (1 to 197)
+    """
+    receptor_prefix = receptor_id[:3]
+    receptor_number = receptor_id[3:]
+    if receptor_prefix != "SKA" and receptor_prefix != "MKT":
+        raise ValueError(
+            "Incorrect DISH type prefix. Prefix must be SKA or MKT."
+        )
+    if len(receptor_number) != 3:
+        raise ValueError(
+            "Incorrect DISH instance size. Dish instance must be a 3 digit number."
+        )
+    if receptor_prefix == "SKA":
+        if 1 > int(receptor_number) > 133:
+            raise ValueError(
+                "Incorrect DISH instance. Dish instance for SKA DISH type is 1 to 133 incl."
+            )
+        else:
+            return receptor_number
+    if receptor_prefix == "MKT":
+        if 0 > int(receptor_number) > 63:
+            raise ValueError(
+                "Incorrect DISH instance. Dish instance for MKT DISH type is 0 to 63 incl."
+            )
+        else:
+            return receptor_number + 134
+
+
+def receptor_id_int_to_str(receptor_id: int) -> str:
+    """
+    Convert DISH/receptor ID integer to mnemonic string.
+
+    :param receptor_id: the DISH/receptor ID as an integer
+
+    :return: the DISH/receptor ID mnemonic as a string
+    """
+    if 1 > receptor_id > 197:
+        raise ValueError(
+            "Incorrect receptor instance. ID should be in the range 1 to 197."
+        )
+    if receptor_id < 134:
+        return f"SKA{receptor_id : 03}"
+    else:
+        return f"MKT{receptor_id - 134 : 03}"
