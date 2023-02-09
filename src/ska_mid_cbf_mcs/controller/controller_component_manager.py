c61e7dacce7e698259477483ee9adf47b67cede1
Mae Yee <mae.yee@mda.space>
2023-02-09 21:47:26 +0000

    CIP-1308 Fix indentation mistake

--- src/ska_mid_cbf_mcs/controller/controller_component_manager.py
+++ src/ska_mid_cbf_mcs/controller/controller_component_manager.py
@@ -727,17 +727,17 @@ class ControllerComponentManager(CbfComponentManager):
                     self._logger.error(log_msg)
                     return (ResultCode.FAILED, log_msg)
 
-                try:
-                    self._group_subarray.command_inout("On")
-                    self._group_vcc.command_inout("On")
-                    self._group_fsp.command_inout("On")
-                except tango.DevFailed:
-                    log_msg = "Failed to turn on group proxies"
-                    self._logger.error(log_msg)
-                    return (ResultCode.FAILED, log_msg)
+            try:
+                self._group_subarray.command_inout("On")
+                self._group_vcc.command_inout("On")
+                self._group_fsp.command_inout("On")
+            except tango.DevFailed:
+                log_msg = "Failed to turn on group proxies"
+                self._logger.error(log_msg)
+                return (ResultCode.FAILED, log_msg)
 
-                message = "CbfController On command completed OK"
-                return (ResultCode.OK, message)
+            message = "CbfController On command completed OK"
+            return (ResultCode.OK, message)
 
         else:
             log_msg = "Proxies not connected"
