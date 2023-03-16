a993139c8a7746fbade2c0e3adee4c7755b09677
taylorhuang <taylorw.huang@gmail.com>
2023-03-16 14:58:16 -0700

    CIP-1257 fix lint subarray component manager test

--- tests/unit/subarray/conftest.py
+++ tests/unit/subarray/conftest.py
@@ -509,7 +509,6 @@ def subarray_component_manager(
             "mid_csp_cbf/fspPstSubarray/03_01",
             "mid_csp_cbf/fspPstSubarray/04_01",
         ],
-        receptor_json_file="receptor_id_dict_4r.json",
         logger=logger,
         push_change_event_callback=push_change_event_callback,
         component_resourced_callback=component_callback,
