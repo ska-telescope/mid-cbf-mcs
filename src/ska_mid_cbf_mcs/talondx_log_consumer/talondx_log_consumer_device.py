a38bb02b7d995ddce92a7f7ae5ac9f88cf5a18e7
tarakong <tara.kong@mda.space>
2023-05-23 16:32:05 +0000

    reformat imports

--- src/ska_mid_cbf_mcs/talondx_log_consumer/talondx_log_consumer_device.py
+++ src/ska_mid_cbf_mcs/talondx_log_consumer/talondx_log_consumer_device.py
@@ -17,7 +17,6 @@ from typing import List
 # tango imports
 import tango
 from ska_tango_base import SKABaseDevice
-from ska_tango_base.base.component_manager import BaseComponentManager
 
 # Additional import
 # PROTECTED REGION ID(TalonDxLogConsumer.additional_import) ENABLED START #
@@ -25,6 +24,7 @@ from ska_tango_base.base.base_device import (
     _LMC_TO_PYTHON_LOGGING_LEVEL,
     _Log4TangoLoggingLevel,
 )
+from ska_tango_base.base.component_manager import BaseComponentManager
 from ska_tango_base.commands import BaseCommand, ResultCode
 from ska_tango_base.control_model import LoggingLevel
 from ska_tango_base.faults import LoggingLevelError
