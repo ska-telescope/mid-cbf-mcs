2ab5de2b200c09c36818c077246b91881830bf02
Mae <mae.yee@mda.space>
2023-06-06 22:02:11 +0000

    CIP-1341 Move instantiation of talon_lru proxies to the On command, to prevent errors in the CBF Controller logs for LRUs that are not the ones relevant to the talon boards under test

--- src/ska_mid_cbf_mcs/controller/controller_component_manager.py
+++ src/ska_mid_cbf_mcs/controller/controller_component_manager.py
@@ -378,17 +378,14 @@ class ControllerComponentManager(CbfComponentManager):
             return
 
         for fqdn in (
-            self._fqdn_fsp + self._fqdn_talon_lru + self._fqdn_subarray
+            self._fqdn_fsp + self._fqdn_subarray
         ):
+
             if fqdn not in self._proxies:
                 try:
                     log_msg = f"Trying connection to {fqdn}"
                     self._logger.info(log_msg)
                     proxy = CbfDeviceProxy(fqdn=fqdn, logger=self._logger)
-
-                    if fqdn in self._fqdn_talon_lru:
-                        proxy.set_timeout_millis(10000)
-
                     self._proxies[fqdn] = proxy
                 except tango.DevFailed as df:
                     self._connected = False
@@ -464,6 +461,7 @@ class ControllerComponentManager(CbfComponentManager):
         :param value: attribute value
         :param quality: attribute quality
         """
+
         if value is not None:
             try:
                 if "healthstate" in name:
@@ -709,7 +707,6 @@ class ControllerComponentManager(CbfComponentManager):
                     talon_lru_fqdn_set.add(config_command["talon_lru_fqdn"])
                 self._logger.info(f"talonlru list = {talon_lru_fqdn_set}")
 
-                # TODO: handle subscribed events for missing LRUs
                 self._fqdn_talon_lru = list(talon_lru_fqdn_set)
             else:
                 # use a hard-coded example fqdn talon lru for simulation mode
@@ -717,6 +714,25 @@ class ControllerComponentManager(CbfComponentManager):
 
             try:
                 for fqdn in self._fqdn_talon_lru:
+                
+                    if fqdn not in self._proxies:
+                        try:
+                            log_msg = f"Trying connection to {fqdn}"
+                            self._logger.info(log_msg)
+                            proxy = CbfDeviceProxy(fqdn=fqdn, logger=self._logger)
+                            proxy.set_timeout_millis(10000)
+                            self._proxies[fqdn] = proxy
+                            # establish proxy connection to component
+                            self._proxies[fqdn].adminMode = AdminMode.ONLINE
+                        except tango.DevFailed as df:
+                            self._connected = False
+                            for item in df.args:
+                                log_msg = (
+                                    f"Failure in connection to {fqdn}; {item.reason}"
+                                )
+                                self._logger.error(log_msg)
+                            return
+
                     self._proxies[fqdn].On()
             except tango.DevFailed:
                 log_msg = "Failed to power on Talon boards"
@@ -785,7 +801,6 @@ class ControllerComponentManager(CbfComponentManager):
                         )
                     self._logger.info(f"talonlru list = {talon_lru_fqdn_set}")
 
-                    # TODO: handle subscribed events for missing LRUs
                     self._fqdn_talon_lru = list(talon_lru_fqdn_set)
             else:
                 # use a hard-coded example fqdn talon lru for simulation mode
