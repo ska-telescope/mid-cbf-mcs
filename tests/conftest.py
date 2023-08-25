a7d387ab0cce311cebbb2b146dd1db423cbbb3bd
So <jso@rmdskadevdu002.mda.ca>
2023-08-25 17:42:12 +0000

    CIP-1677 add the third power supply proxy

--- tests/conftest.py
+++ tests/conftest.py
@@ -468,7 +468,7 @@ def init_proxies_fixture():
 
             # Power switch
             self.power_switch = []
-            for i in range(1, 3):  # 2 Power Switches
+            for i in range(1, 4):  # 3 Power Switches
                 self.power_switch.append(
                     CbfDeviceProxy(
                         fqdn=f"mid_csp_cbf/power_switch/{i:03}",
