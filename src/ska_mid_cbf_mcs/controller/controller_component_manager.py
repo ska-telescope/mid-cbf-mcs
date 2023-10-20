d6b87e77d3f45942f24e4a02f2600f420ec7bc8c
tarakong <tara.kong@mda.space>
2023-10-20 21:08:50 +0000

    CIP-1673 try reading State attribute instead of sending State() command

--- src/ska_mid_cbf_mcs/controller/controller_component_manager.py
+++ src/ska_mid_cbf_mcs/controller/controller_component_manager.py
@@ -659,18 +659,18 @@ class ControllerComponentManager(CbfComponentManager):
                     # On/Off commands, rather TurnOn/OffOutlet commands to
                     # target specific outlets
                     if fqdn not in self._fqdn_power_switch:
-                        proxy_State = proxy.State()
-                        proxy_state = proxy.state()
-                        proxy_Status = proxy.Status()
-                        proxy_status = proxy.status()
-                        self._logger.info(f"proxy.State() is {proxy_State}")
-                        self._logger.info(f"proxy.state() is {proxy_state}")
-                        self._logger.info(f"proxy.Status() is {proxy_Status}")
-                        self._logger.info(f"proxy.status() is {proxy_status}")
-                        if proxy_State != tango.DevState.OFF:
+                        # proxy_State = proxy.State()
+                        # proxy_state = proxy.state()
+                        # proxy_Status = proxy.Status()
+                        # proxy_status = proxy.status()
+                        # self._logger.info(f"proxy.State() is {proxy_State}")
+                        # self._logger.info(f"proxy.state() is {proxy_state}")
+                        # self._logger.info(f"proxy.Status() is {proxy_Status}")
+                        # self._logger.info(f"proxy.status() is {proxy_status}")
+                        if proxy.State != tango.DevState.OFF:
                             try:
                                 poll(
-                                    lambda: proxy.State()
+                                    lambda: proxy.State
                                     == tango.DevState.OFF,
                                     timeout=const.DEFAULT_TIMEOUT,
                                     step=0.5,
