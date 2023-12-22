82b8f51d3eeac2a5b2b3fe717535ba2d0a4de8a8
Edna Sanchez <edna.sanchez@mda.space>
2023-12-22 22:30:34 +0000

    CIP-2004 mcs integration cbf subarray test comment out unnecessary code

--- tests/integration/subarray/CbfSubarray_test.py
+++ tests/integration/subarray/CbfSubarray_test.py
@@ -1927,25 +1927,23 @@ class TestCbfSubarray:
                     # It is assumed that there is only one entry in the
                     # delay model for a given receptor
                     dm_found = False
-                    for model in input_delay_model_obj["models"]:
-                        for entry in model["model"]["delay_details"]:
-                            if entry["receptor"] == rec:
-                                this_input_delay_model_obj = copy.deepcopy(
-                                    entry
-                                )
-                                # receptor as pair of str and int for comparison
-                                this_input_delay_model_obj["receptor"] = [
-                                    entry["receptor"],
-                                    test_proxies.receptor_utils.receptor_id_to_vcc_id[
-                                        entry["receptor"]
-                                    ],
-                                ]
-                                dm_found = True
-                                break
-                        if dm_found:
+                    # for model in input_delay_model_obj["models"]:
+                    for entry in input_delay_model["delay_details"]:
+                        if entry["receptor"] == rec:
+                            this_input_delay_model_obj = copy.deepcopy(entry)
+                            # receptor as pair of str and int for comparison
+                            this_input_delay_model_obj["receptor"] = [
+                                entry["receptor"],
+                                test_proxies.receptor_utils.receptor_id_to_vcc_id[
+                                    entry["receptor"]
+                                ],
+                            ]
+                            dm_found = True
                             break
                     if dm_found:
                         break
+                    # if dm_found:
+                    #    break
 
                     print(f"vcc delay model {this_vcc}: {vcc_dp.delayModel}")
                     vcc_updated_delayModel_obj = json.loads(vcc_dp.delayModel)
