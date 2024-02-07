6c51fc9159312e0be2c427fa5359a09eff75d9a7
Huang <taylor.huang@mda.space>
2024-02-07 17:46:58 +0000

    CIP-1767 fix fsp pst unit test

--- tests/unit/fsp_pst_subarray_component_manager/FspPstSubarrayComponentManager_test.py
+++ tests/unit/fsp_pst_subarray_component_manager/FspPstSubarrayComponentManager_test.py
@@ -162,7 +162,7 @@ class TestFspPstSubarrayComponentManager:
         assert fsp_pst_subarray_component_manager.timing_beam_id == []
         assert fsp_pst_subarray_component_manager.timing_beams == []
         assert fsp_pst_subarray_component_manager._output_enable == 0
-        assert fsp_pst_subarray_component_manager.receptors == []
+        assert fsp_pst_subarray_component_manager.vcc_ids == []
 
     @pytest.mark.parametrize(
         "config_file_name",
