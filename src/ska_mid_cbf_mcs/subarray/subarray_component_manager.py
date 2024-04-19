9c65088a3eaeca619e8eef24b18f303fb9100dc1
Sheldon Downie <Sheldon.Downie@mda.space>
2024-04-19 20:25:25 +0000

    lint fix

--- src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
@@ -480,7 +480,9 @@ class CbfSubarrayComponentManager(
                 delay_model_json = json.loads(value)
 
                 # Validate delay_model_json against the telescope model
-                print(f"CIP-2257 test output - attempting to validate the following json against the telescope model: {delay_model_json}")
+                print(
+                    f"CIP-2257 test output - attempting to validate the following json against the telescope model: {delay_model_json}"
+                )
                 try:
                     telmodel_validate(
                         version=delay_model_json["interface"],
