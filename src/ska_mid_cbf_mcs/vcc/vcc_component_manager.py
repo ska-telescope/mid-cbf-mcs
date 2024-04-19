818f6b198faeaebcc108a6d734269898b9e434b1
Sheldon Downie <sheldon.downie@mda.space>
2024-04-19 23:48:24 +0000

    CIP-2257 Update delay model json to version 3.0

--- src/ska_mid_cbf_mcs/vcc/vcc_component_manager.py
+++ src/ska_mid_cbf_mcs/vcc/vcc_component_manager.py
@@ -914,7 +914,7 @@ class VccComponentManager(CbfComponentManager, CspObsComponentManager):
 
         # Note: subarray has translated DISH IDs to VCC IDs in the JSON at this point
         list_of_entries = []
-        for entry in delay_model_obj["delay_details"]:
+        for entry in delay_model_obj["receptor_delays"]:
             self._logger.debug(
                 f"Received delay model for VCC {entry['receptor']}"
             )
@@ -922,7 +922,7 @@ class VccComponentManager(CbfComponentManager, CspObsComponentManager):
                 self._logger.debug("Updating delay model for this VCC")
                 list_of_entries.append(copy.deepcopy(entry))
                 self._delay_model = json.dumps(
-                    {"delay_details": list_of_entries}
+                    {"receptor_delays": list_of_entries}
                 )
                 dm_found = True
                 break
