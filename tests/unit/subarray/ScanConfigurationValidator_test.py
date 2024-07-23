51f3e069238a778c74c8a0bc0451956079a25104
Acky Xu <ackyzirui.xu@mda.space>
2024-07-23 21:00:57 +0000

    CIP-2616: Refactor Scan Configuration Validator Class to Subarray Component Manager; Abstracted previous scan configuration validate functions; init commit for 4.0 validate functions

--- tests/unit/subarray/ScanConfigurationValidator_test.py
+++ tests/unit/subarray/ScanConfigurationValidator_test.py
@@ -4,7 +4,7 @@ import os
 from logging import getLogger
 
 import pytest
-from src.ska_mid_cbf_mcs.commons.scan_configuration_validator import ScanConfigurationValidator
+from src.ska_mid_cbf_mcs.subarray.subarray_component_manager import CbfSubarrayComponentManager, ScanConfigurationValidator
 
 # Path
 file_path = os.path.dirname(os.path.abspath(__file__)) + "/../../data/"
@@ -17,11 +17,12 @@ class TestScanConfigurationValidator:
     )
     def test_Invalid_Configuration_Version(
             self:TestScanConfigurationValidator,
+            subarray_component_manager: CbfSubarrayComponentManager,
             config_file_name: str):
         path_to_test_json = os.path.join(file_path,config_file_name)
         with open(path_to_test_json) as file:
             json_str = file.read().replace("\n", "")
-        validator:ScanConfigurationValidator = ScanConfigurationValidator(json_str, self.logger)
+        validator:ScanConfigurationValidator = ScanConfigurationValidator(json_str, subarray_component_manager,self.logger)
         result_code, msg = validator.validate_input()
         print(msg)
         assert("The version defined in the Scan Configuration is not supported by MCS:" in msg)
@@ -29,16 +30,32 @@ class TestScanConfigurationValidator:
     
     
     @pytest.mark.parametrize(
-        "config_file_name", [("ConfigureScan_4_0_CORR.json"),
-                             ("ConfigureScan_basic_CORR.json")]
+        "config_file_name,\
+         receptors", 
+        [("ConfigureScan_4_0_CORR.json",
+          ["SKA001", "SKA036", "SKA063", "SKA100"],),
+         ("ConfigureScan_basic_CORR.json",
+          ["SKA001", "SKA036", "SKA063", "SKA100"],)]
     )  
     def test_Valid_Configuration_Version(
             self:TestScanConfigurationValidator,
-            config_file_name: str):
+            subarray_component_manager: CbfSubarrayComponentManager,
+            config_file_name: str,
+            receptors: list[str]):
         path_to_test_json = os.path.join(file_path,config_file_name)
+        
+        subarray_component_manager.start_communicating()
+
+        with open(file_path + "sys_param_4_boards.json") as f:
+            sp = f.read()
+        subarray_component_manager.update_sys_param(sp)
+
+        subarray_component_manager.assign_vcc(receptors)
+        
         with open(path_to_test_json) as file:
             json_str = file.read().replace("\n", "")
-        validator:ScanConfigurationValidator = ScanConfigurationValidator(json_str, self.logger)
+        validator:ScanConfigurationValidator = ScanConfigurationValidator(json_str, subarray_component_manager,self.logger)
         result_code, msg = validator.validate_input()
         print(msg)
+        assert("Scan configuration is valid." in msg)
         assert(result_code == True)
