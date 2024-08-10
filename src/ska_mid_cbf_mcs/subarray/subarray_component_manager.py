b8f59974567d62c5e878642db509e5a89f226723
Devan Yim <devan.yim.ska@gmail.com>
2024-08-10 05:29:35 +0000

    CIP-2554 remove unused import

--- src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
@@ -34,10 +34,6 @@ from ska_control_model import (
 )
 from ska_tango_base.base.base_component_manager import check_communicating
 from ska_tango_testing import context
-from ska_telmodel.csp.common_schema import (
-    MAX_CHANNELS_PER_STREAM,
-    MAX_STREAMS_PER_FSP,
-)
 from ska_telmodel.schema import validate as telmodel_validate
 
 from ska_mid_cbf_mcs.commons.dish_utils import DISHUtils
