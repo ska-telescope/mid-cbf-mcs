249d05bbe32671e4e97d4d6df4d84a3ec367e54f
Bryan Huang <bryan.huang.ska@gmail.com>
2024-08-13 16:49:08 +0000

    CIP-2423 fix concat

--- src/ska_mid_cbf_mcs/controller/controller_component_manager.py
+++ src/ska_mid_cbf_mcs/controller/controller_component_manager.py
@@ -1137,11 +1137,12 @@ class ControllerComponentManager(CbfComponentManager):
             # communicating and monitoring the PDU; does not implement
             # On/Off commands, rather TurnOn/OffOutlet commands to
             # target specific outlets
-            if fqdn not in [
-                self._power_switch_fqdn,
-                self._subarray_fqdn,
-                self._vcc_fqdn,
-            ]:
+            if (
+                fqdn
+                not in self._power_switch_fqdn
+                + self._subarray_fqdn
+                + self._vcc_fqdn
+            ):
                 try:
                     # TODO: CIP-1899 The cbfcontroller is sometimes
                     # unable to read the State() of the talon_lru
