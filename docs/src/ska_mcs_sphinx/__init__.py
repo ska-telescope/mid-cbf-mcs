e48da9a15a2deb8625dedec904fa47c35d3056aa
danielpoonmda <daniel.poon@mda.space>
2024-08-15 21:27:54 +0000

    CIP-2732: Update init

--- docs/src/ska_mcs_sphinx/__init__.py
+++ docs/src/ska_mcs_sphinx/__init__.py
@@ -4,5 +4,4 @@ from . import ska_tables
 def setup(app):
     app.add_directive("cbf-controller-table", ska_tables.CbfControllerTable)
     app.add_directive("cbf-subarray-table", ska_tables.CbfSubarrayTable)
-    app.add_directive("hello", ska_tables.HelloDirective)
     return {"version": "0.1.0"}
