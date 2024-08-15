1cb67f1f77b5578d71d6395247b228604e7bd734
Devan Yim <devan.yim.ska@gmail.com>
2024-08-15 19:29:04 +0000

    without with cleandoc and no method use

--- docs/src/ska_mcs_sphinx/ska_tables.py
+++ docs/src/ska_mcs_sphinx/ska_tables.py
@@ -17,13 +17,15 @@ import importlib
 
 HEADER_LIST = ['Command', 'Parameters', 'Return type', 'Action', 'Supported Interface']
 
-test_reference_string = """
-    Text
-    
-    Some values
-    
-    See also :ref:`Abort Sequence`
+test_reference_string = cleandoc(
     """
+    | Text\n
+    | \n
+    | Some values\n
+    | \n
+    | See also :ref:`Abort Sequence`\n
+    """
+)
 
 # Variables: num_rows, command_list, param_list, return_list, action_list, supported_versions_list
 # TODO: For supported versions we can read param list and if json is found we can
@@ -72,8 +74,7 @@ class SkaTables(Directive):
         r1_c4_entry = nodes.entry('', nodes.paragraph(text='no'))
         r1_c5_entry = nodes.entry('', nodes.paragraph(text='no'))
         
-        r1_c5_entry = nodes.entry('')          # Test if this works in rst
-        r1_c5_entry.children = self._parse_text(test_reference_string)
+        r1_c5_entry = nodes.entry('', nodes.paragraph(text=test_reference_string))          # Test if this works in rst
         
         row1  +=  (r1_c1_entry)
         row1  +=  (r1_c2_entry)
@@ -92,7 +93,11 @@ class SkaTables(Directive):
         tgroup  +=  (header)
         tgroup  +=  (table_body)
         
-        return [table]
+        node = nodes.section()
+        node.document = self.state.document
+        
+        nested_parse_with_titles(self.state, table, node)
+        return node.children
 
 
     def _parse_text(self, text_to_parse: str):
