16329f4189ecd2a792228b4dbe825bc050789b68
Owen Douglas <owen.douglas@mda.space>
2024-08-21 18:46:37 +0000

    CIP-1924 lint

--- src/ska_mid_cbf_mcs/power_switch/apc_snmp_driver.py
+++ src/ska_mid_cbf_mcs/power_switch/apc_snmp_driver.py
@@ -70,7 +70,7 @@ class ApcSnmpDriver:
 
         # valid range 1 to 24
         self.outlet_id_list: list[str] = [
-            str(i) for i in range(1, self.power_switch_outlets+1)
+            str(i) for i in range(1, self.power_switch_outlets + 1)
         ]
 
         # Initialize outlets
