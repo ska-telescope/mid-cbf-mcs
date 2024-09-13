7b77f02e0c6707ce1e66d024e43b07445db7ec3f
Devan Yim <devan.yim.ska@gmail.com>
2024-09-13 06:42:55 +0000

    CIP-2254 test configurescan table

--- docs/src/ska_mcs_sphinx/table_data/configscan_validation.py
+++ docs/src/ska_mcs_sphinx/table_data/configscan_validation.py
@@ -3,8 +3,34 @@ from inspect import cleandoc
 configurescan_validation_rules = [
     {
         "Scan Configuration Parameter": "",
-        "AA4 Ranges (see TM)": "",
-        "Supported": "",
-        "Comment": "",
+        "AA4 Ranges (see TM)": cleandoc(
+            """
+            some ``formated`` text here.
+            
+            some *italic* text here.
+            
+            some **bold** text here.
+            """),
+        "Supported": cleandoc(
+            """
+            .. warning::
+                This is a warning block
+            
+                This value is not supported yet!
+            """),
+        "Comment": cleandoc(
+            """
+            complex check:
+            
+            #. check 1
+            #. check 2
+            
+               * Sub-point 1
+
+               * Sub-point 2
+            
+            #. check 3
+            
+            """),
     }
 ]
