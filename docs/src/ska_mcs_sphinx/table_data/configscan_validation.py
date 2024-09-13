b525eeabe8129ad7a9b0a5c7fc4f1c61732ed41d
Devan Yim <devan.yim.ska@gmail.com>
2024-09-13 08:46:18 +0000

    CIP-2254 use exact data for second row

--- docs/src/ska_mcs_sphinx/table_data/configscan_validation.py
+++ docs/src/ska_mcs_sphinx/table_data/configscan_validation.py
@@ -43,10 +43,38 @@ configurescan_validation_rules = {
                 """),
         },
         {
-            "Scan Configuration Parameter": "other",
-            "AA4 Ranges (see TM)": "some value",
-            "Supported": "a subset of the value",
-            "Comment": "no comment",
-        },
+            "Scan Configuration Parameter": "test2",
+            "AA4 Ranges (see TM)": cleandoc(
+                """
+                some ``formated`` text here.
+                
+                some *italic* text here.
+                
+                some **bold** text here.
+                """),
+            "Supported": cleandoc(
+                """
+                this is a literal block::
+
+                    This is the first line of a ``not processed`` line
+                    
+                    This is the **second** line which is not *processed*
+                    
+                This is normal text again
+                """),
+            "Comment": cleandoc(
+                """
+                a list of items:
+                
+                #. check 1
+                #. check 2
+                
+                   * Sub-point 1
+                   * Sub-point 2
+                
+                #. check 3
+                
+                """),
+        }
     ]
 }
