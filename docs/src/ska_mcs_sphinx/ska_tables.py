6a0ad349640ba5346d524f5ddf8d7381b3dbba4c
Devan Yim <devan.yim.ska@gmail.com>
2024-09-13 06:30:28 +0000

    CIP-2254 fix parsing text

--- docs/src/ska_mcs_sphinx/ska_tables.py
+++ docs/src/ska_mcs_sphinx/ska_tables.py
@@ -65,7 +65,9 @@ class CommandTable(Directive):
                     supported_entry.append(self._create_line_block_from_list(col_data))
                     row.append(supported_entry)
                 else:
-                    row.append(nodes.entry('', self._parse_text(col_data)))
+                    col_entry = nodes.entry()
+                    col_entry.children = self._parse_text(col_data)
+                    row.append(col_entry)
 
             table_body.append(row)
 
