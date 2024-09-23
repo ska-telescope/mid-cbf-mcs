f624d1b3aa76ec644968f1154ca8290ca7f29848
Devan Yim <devan.yim.ska@gmail.com>
2024-09-23 18:41:51 +0000

    CIP-2254 reorder and clean up table data

--- docs/src/ska_mcs_sphinx/table_data/configscan_validation.py
+++ docs/src/ska_mcs_sphinx/table_data/configscan_validation.py
@@ -9,31 +9,6 @@ configurescan_validation_rules_data = {
     ], 
     "data": [
         {
-            "Scan Configuration Parameter": "subarray_id",
-            "AA4 Ranges (see TM)": "1 - 16",
-            "Supported": "1",
-            "Comment": "",
-        },
-        {
-            "Scan Configuration Parameter": "fsp_ids",
-            "AA4 Ranges (see TM)": "Array of 1 to 26 fsp_id",
-            "Supported": "Array of 1 to 4 fsp_id",
-            "Comment": "",
-        },
-        {
-            "Scan Configuration Parameter": "fsp_id",
-            "AA4 Ranges (see TM)": "1 - 27",
-            "Supported": cleandoc(
-                """
-                Complex:
-                
-                * if CORR: 1 - 4
-                
-                """
-                ),
-            "Comment": "PST: 5 - 8 when implemented",
-        },
-        {
             "Scan Configuration Parameter": "band_5_tuning",
             "AA4 Ranges (see TM)": "See TM",
             "Supported": "None",
@@ -48,9 +23,30 @@ configurescan_validation_rules_data = {
                 """),
         },
         {
+            "Scan Configuration Parameter": "channel_count",
+            "AA4 Ranges (see TM)": "See TM",
+            "Supported": cleandoc(
+                """
+                Range: Integer from 1 to 58982 inclusive
+                
+                Checks:
+                
+                * Must be a multiple of 20
+                
+                """
+                ),
+            "Comment": "",
+        },
+        {
+            "Scan Configuration Parameter": "channel_width",
+            "AA4 Ranges (see TM)": "See TM for enum",
+            "Supported": "Integer of exactly 13440",
+            "Comment": "",
+        },
+        {
             "Scan Configuration Parameter": "frequency_band",
-            "AA4 Ranges (see TM)": "1, 2, 5a, 5b",
-            "Supported": "1, 2",
+            "AA4 Ranges (see TM)": "Strings of values: 1, 2, 5a, 5b",
+            "Supported": "Strings of values: 1, 2",
             "Comment": "",
         },
         {
@@ -82,6 +78,74 @@ configurescan_validation_rules_data = {
                 """),
         },
         {
+            "Scan Configuration Parameter": "fsp_id",
+            "AA4 Ranges (see TM)": "Integer from 1 to 27 inclusive",
+            "Supported": cleandoc(
+                """
+                Checks:
+                
+                * if ``CORR``: Integer from 1 to 4 inclusive
+                """
+                ),
+            "Comment": "``PST``: 5 - 8 when implemented",
+        },
+        {
+            "Scan Configuration Parameter": "fsp_ids",
+            "AA4 Ranges (see TM)": "Array of 1 to 26 ``fsp_id``",
+            "Supported": "Array of 1 to 4 ``fsp_id``",
+            "Comment": "",
+        },
+        {
+            "Scan Configuration Parameter": "integration_factor",
+            "AA4 Ranges (see TM)": "Integer from 1 - 10 inclusive",
+            "Supported": "Integer from 1 - 10 inclusive",
+            "Comment": "No additional checks needed",
+        },
+        {
+            "Scan Configuration Parameter": "output_link_map",
+            "AA4 Ranges (see TM)": cleandoc(
+                """
+                Ranges: See TM
+                
+                Checks:
+                
+                * First entry ``start_channel_id`` must match the ``sdp_start_channel_id``
+                
+                """),
+            "Supported": "[[``sdp_start_channel_id``, 1]]",
+            "Comment": "",
+        },
+        {
+            "Scan Configuration Parameter": "output_port",
+            "AA4 Ranges (see TM)": cleandoc(
+                """
+                Ranges: See TM
+                
+                Checks:
+                
+                * First entry ``start_channel_id`` must match the ``sdp_start_channel_id``
+
+                """),
+            "Supported": cleandoc(
+                """
+                In addition to AA4 ranges:
+                
+                * ``start_channel_id``'s must be in ascending order
+                
+                * ``start_channel_id``'s must be multiples of 20
+                
+                * At most 20 chanels can be sent to the same port per host
+
+                """),
+            "Comment": "",
+        },
+        {
+            "Scan Configuration Parameter": "receptors",
+            "AA4 Ranges (see TM)": "all SKA and MKT",
+            "Supported": "all",
+            "Comment": "No additional checks needed",
+        },
+        {
             "Scan Configuration Parameter": "rfi_flagging_mask",
             "AA4 Ranges (see TM)":  "See TM",
             "Supported": "None",
@@ -96,71 +160,13 @@ configurescan_validation_rules_data = {
                 """),
         },
         {
-            "Scan Configuration Parameter": "receptors",
-            "AA4 Ranges (see TM)": "all SKA and MKT",
-            "Supported": "all",
-            "Comment": "No additional check needed",
-        },
-        {
-            "Scan Configuration Parameter": "start_freq",
-            "AA4 Ranges (see TM)":  "See TM",
-            "Supported":  cleandoc(
-                """
-                Complex check
-
-                * Entire processing region must fall inside specified band
-                
-                * Required bandwidth [start_freq - (1/2 * start_freq) +
-                (channel_width * channel_count)] must not require more FSP's
-                than provided in fsp_ids
-
-                """),
-            "Comment": "",
-        },
-        {
-            "Scan Configuration Parameter": "channel_width",
-            "AA4 Ranges (see TM)": "See TM for enumT",
-            "Supported": "13440",
-            "Comment": "",
-        },
-        {
-            "Scan Configuration Parameter": "channel_count",
-            "AA4 Ranges (see TM)": "See TM",
-            "Supported": cleandoc(
-                """
-                * Integer from 1 to 58982 inclusive
-                
-                * Must be a multiple of 20
-                """
-                ),
-            "Comment": "",
-        },
-        {
             "Scan Configuration Parameter": "sdp_start_channel_id",
             "AA4 Ranges (see TM)": "See TM",
             "Supported": "Same as TM",
-            "Comment": "No additional check needed",
+            "Comment": "No additional checks needed",
         },
         {
-            "Scan Configuration Parameter": "integration_factor",
-            "AA4 Ranges (see TM)": "1 - 10",
-            "Supported": "1 - 10",
-            "Comment": "No additional check needed",
-        },
-        {
-            "Scan Configuration Parameter": "output_port",
-            "AA4 Ranges (see TM)": "1 - 10",
-            "Supported": "1 - 10",
-            "Comment": "",
-        },
-        {
-            "Scan Configuration Parameter": "output_link_map",
-            "AA4 Ranges (see TM)": "1 - 10",
-            "Supported": "[[sdp_start_channel_id, 1]]",
-            "Comment": "",
-        },
-        {
-            "Scan Configuration Parameter": "vlbi",
+            "Scan Configuration Parameter": "search_window",
             "AA4 Ranges (see TM)": "",
             "Supported": "None",
             "Comment": cleandoc(
@@ -174,7 +180,29 @@ configurescan_validation_rules_data = {
                 """),
         },
         {
-            "Scan Configuration Parameter": "search_window",
+            "Scan Configuration Parameter": "start_freq",
+            "AA4 Ranges (see TM)":  "See TM",
+            "Supported":  cleandoc(
+                """
+                Checks:
+
+                * Entire processing region must fall inside specified band
+                
+                * Required bandwidth [``start_freq`` - (1/2 * ``start_freq``) +
+                (``channel_width`` * ``channel_count``)] must not require more
+                FSP's than provided in ``fsp_ids``
+
+                """),
+            "Comment": "",
+        },
+        {
+            "Scan Configuration Parameter": "subarray_id",
+            "AA4 Ranges (see TM)": "Integer from 1 to 16 inclusive",
+            "Supported": "Integer of value 1",
+            "Comment": "",
+        },
+        {
+            "Scan Configuration Parameter": "vlbi",
             "AA4 Ranges (see TM)": "",
             "Supported": "None",
             "Comment": cleandoc(
