386ab66eb3199f47bb4fd0ec6c16e71068f6c9b2
Bryan Huang <bryan.huang.ska@gmail.com>
2024-10-30 17:36:23 +0000

    CIP-2550 fix talondx_config_json read

--- src/ska_mid_cbf_mcs/controller/controller_component_manager.py
+++ src/ska_mid_cbf_mcs/controller/controller_component_manager.py
@@ -398,43 +398,42 @@ class ControllerComponentManager(CbfComponentManager):
 
         :return: True if the FSP function mode is successfully set, False otherwise.
         """
-        try:
-            fsp_mode = self.talondx_config_json["config_commands"][
-                "fpga_bitstream_fsp_mode"
-            ].upper()
-            self.logger.info(f"Setting FSP function mode to {fsp_mode}")
 
-            for fsp in self._fsp_fqdn:
-                try:
-                    # Only set function mode if FSP is both IDLE and not configured for another mode
-                    fsp_proxy = self._proxies[fsp]
-                    current_function_mode = fsp_proxy.functionMode
-                    if current_function_mode != FspModes[fsp_mode].value:
-                        if current_function_mode != FspModes.IDLE.value:
-                            self.logger.error(
-                                f"Unable to configure FSP {fsp_proxy.dev_name()} for function mode {fsp_mode}, as it is currently configured for function mode {current_function_mode}"
-                            )
-                            return False
+        fsp_mode = self.talondx_config_json["config_commands"][0][
+            "fpga_bitstream_fsp_mode"
+        ].upper()
+        self.logger.info(f"Setting FSP function mode to {fsp_mode}")
 
-                    [[result_code], [command_id]] = fsp_proxy.SetFunctionMode(
-                        fsp_mode
-                    )
-                    if result_code == ResultCode.REJECTED:
+        for fsp in self._fsp_fqdn:
+            try:
+                # Only set function mode if FSP is both IDLE and not configured for another mode
+                fsp_proxy = self._proxies[fsp]
+                current_function_mode = fsp_proxy.functionMode
+                if current_function_mode != FspModes[fsp_mode].value:
+                    if current_function_mode != FspModes.IDLE.value:
                         self.logger.error(
-                            f"{fsp_proxy.dev_name()} SetFunctionMode command rejected"
+                            f"Unable to configure FSP {fsp_proxy.dev_name()} for function mode {fsp_mode}, as it is currently configured for function mode {current_function_mode}"
                         )
                         return False
-                    self.blocking_command_ids.add(command_id)
-                except Exception as e:
+
+                [[result_code], [command_id]] = fsp_proxy.SetFunctionMode(
+                    fsp_mode
+                )
+
+                if result_code == ResultCode.REJECTED:
                     self.logger.error(
-                        f"Error configuring FSP {fsp}: {e}", exc_info=True
+                        f"{fsp_proxy.dev_name()} SetFunctionMode command rejected"
                     )
                     return False
 
-            return True
-        except Exception as e:
-            self.logger.error(f"Error in _assign_fsp: {e}", exc_info=True)
-            return False
+                self.blocking_command_ids.add(command_id)
+            except Exception as e:
+                self.logger.error(
+                    f"Error configuring FSP {fsp}: {e}", exc_info=True
+                )
+                return False
+
+        return True
 
     def _start_communicating(
         self: ControllerComponentManager, *args, **kwargs
