2635a7a9ebfe03b2be95d4f88387fd66c113b268
John So <john.so@mda.space>
2024-11-01 17:35:56 +0000

    CIP-2991 Moved delay model subscription to after configuring FSP. Accept DMs while configuring.

--- src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
@@ -339,7 +339,10 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
         if not self.is_communicating or model is None or model == "":
             return
 
-        if self.obs_state not in [ObsState.READY, ObsState.SCANNING]:
+        # Subscription starts during the configure scan command. Need to include the
+        # CONFIGURING state or we risk throwing away the first polynomial. Subscription should
+        # also be done after FSPs have been configured so that RDTs are ready to receive them.
+        if self.obs_state not in [ObsState.CONFIGURING, ObsState.READY, ObsState.SCANNING]:
             log_msg = f"Ignoring delay model received in {self.obs_state} (must be READY or SCANNING)."
             self.logger.warning(log_msg)
             return
@@ -1722,22 +1725,6 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
             )
             return
 
-        # Configure delayModel subscription point
-        delay_model_success = self._subscribe_tm_event(
-            subscription_point=configuration["delay_model_subscription_point"],
-            callback=self._delay_model_event_callback,
-        )
-        if not delay_model_success:
-            self.logger.error("Failed to subscribe to TM events.")
-            task_callback(
-                status=TaskStatus.FAILED,
-                result=(
-                    ResultCode.FAILED,
-                    "Failed to subscribe to delayModel attribute",
-                ),
-            )
-            return
-
         # store configID
         self.config_id = str(common_configuration["config_id"])
 
@@ -1792,6 +1779,22 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
                 result=(
                     ResultCode.FAILED,
                     "Failed to issue ConfigureScan command to FSP",
+                ),
+            )
+            return
+        
+        # Configure delayModel subscription point
+        delay_model_success = self._subscribe_tm_event(
+            subscription_point=configuration["delay_model_subscription_point"],
+            callback=self._delay_model_event_callback,
+        )
+        if not delay_model_success:
+            self.logger.error("Failed to subscribe to TM events.")
+            task_callback(
+                status=TaskStatus.FAILED,
+                result=(
+                    ResultCode.FAILED,
+                    "Failed to subscribe to delayModel attribute",
                 ),
             )
             return
