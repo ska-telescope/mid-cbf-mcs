6afe8c78ff9ccea2f27b072e995fdfc6bdacc140
Aditya Joshi <aditya.joshi@mda.space>
2024-11-09 04:23:31 +0000

    MAP-158 Testing subarray vcc id theory

--- src/ska_mid_cbf_mcs/fsp/fsp_corr_subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/fsp/fsp_corr_subarray_component_manager.py
@@ -162,6 +162,8 @@ class FspCorrSubarrayComponentManager(CbfObsComponentManager):
         hps_fsp_configuration["configure_scan"]["subarray_vcc_ids"].sort()
         hps_fsp_configuration["configure_scan"]["corr_vcc_ids"].sort()
 
+        hps_fsp_configuration["configure_scan"]["subarray_vcc_ids"] = hps_fsp_configuration["configure_scan"]["corr_vcc_ids"]
+
         # Get the internal parameters from file
         internal_params_file_name = FSP_CORR_PARAM_PATH
         with open(internal_params_file_name) as f:
