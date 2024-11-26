23bf5f7459b969742a80e322306f291120f65181
danielpoonmda <daniel.poon@mda.space>
2024-11-26 22:33:13 +0000

    CIP-2816: Test count variables

--- src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
@@ -401,7 +401,7 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
             results_fsp = self.issue_group_command(
                 command_name="UpdateDelayModel",
                 proxies=list(self._assigned_fsp_corr_proxies),
-                max_workers=self._controller_max_capabilities["VCC"],
+                max_workers=self._count_vcc,
                 argin=json.dumps(delay_model_json),
             )
 
@@ -961,7 +961,7 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
         for [[result_code], [command_id]] in self.issue_group_command(
             command_name=command_name,
             proxies=list(assigned_resources),
-            max_workers=self._controller_max_capabilities["VCC"],
+            max_workers=self._count_vcc,
             argin=argin,
         ):
             if result_code in [ResultCode.REJECTED, ResultCode.FAILED]:
@@ -1611,7 +1611,7 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
         for [[result_code], [command_id]] in self.issue_group_command(
             command_name="RemoveSubarrayMembership",
             proxies=list(self._assigned_fsp_proxies),
-            max_workers=self._controller_max_capabilities["VCC"],
+            max_workers=self._count_vcc,
             argin=self._subarray_id,
         ):
             if result_code in [ResultCode.REJECTED, ResultCode.FAILED]:
@@ -1731,7 +1731,7 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
         for [[result_code], [command_id]] in self.issue_group_command(
             command_name="GoToIdle",
             proxies=list(self._assigned_fsp_corr_proxies),
-            max_workers=self._controller_max_capabilities["VCC"],
+            max_workers=self._count_vcc,
         ):
             if result_code in [ResultCode.REJECTED, ResultCode.FAILED]:
                 self.logger.error("FSP GoToIdle command failed")
