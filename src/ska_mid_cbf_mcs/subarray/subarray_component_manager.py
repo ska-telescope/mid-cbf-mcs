7cd6e5a2a1fdbf785b8d7dbb03a17119638d858c
Huang <taylor.huang@mda.space>
2024-11-26 23:25:11 +0000

    CIP-3048 move vis transport config around

--- src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/subarray/subarray_component_manager.py
@@ -1922,8 +1922,20 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
             )
             return
 
-        # issue Scan to assigned resources
         scan_id = int(scan["scan_id"])
+
+        # Configure visibility transport
+        if not self.simulation_mode:
+            self.logger.info("Configuring visibility transport")
+            vis_slim_yaml = self._proxy_vis_slim.meshConfiguration
+            self._vis_transport.configure(
+                scan_id=scan_id,
+                subarray_id=self._subarray_id,
+                fsp_config=self._vis_fsp_config,
+                vis_slim_yaml=vis_slim_yaml,
+            )
+
+        # Issue Scan command to assigned resources
         scan_status = self._issue_lrc_all_assigned_resources(
             command_name="Scan",
             argin=scan_id,
@@ -1948,17 +1960,8 @@ class CbfSubarrayComponentManager(CbfObsComponentManager):
             )
             return
 
-        # --- Configure Visibility Transport --- #
-
+        # Enable visibility transport output
         if not self.simulation_mode:
-            self.logger.info("Configuring visibility transport")
-            vis_slim_yaml = self._proxy_vis_slim.meshConfiguration
-            self._vis_transport.configure(
-                scan_id=scan_id,
-                subarray_id=self._subarray_id,
-                fsp_config=self._vis_fsp_config,
-                vis_slim_yaml=vis_slim_yaml,
-            )
             self.logger.info("Visibility transport enable output")
             self._vis_transport.enable_output(self._subarray_id)
 
