e93320ba7a6a49d31332e01f3503586017bd65e7
Owen Douglas <owen.douglas@mda.space>
2024-11-28 19:42:37 +0000

    CIP-2395 removed troubleshooting log

--- src/ska_mid_cbf_mcs/talon_board/talon_board_component_manager.py
+++ src/ska_mid_cbf_mcs/talon_board/talon_board_component_manager.py
@@ -398,9 +398,6 @@ class TalonBoardComponentManager(CbfComponentManager):
             cur_val = getter_fn()
             with self._polled_attr_lock:
                 try:
-                    self.logger.error(
-                        f"{attr_name}: CUR={cur_val}, STORED={self._polled_attr[attr_name]}, TYPE={type(cur_val)}"
-                    )
                     if cur_val != self._polled_attr[attr_name]:
                         self.device_attr_change_callback(attr_name, cur_val)
                         self.device_attr_archive_callback(attr_name, cur_val)
