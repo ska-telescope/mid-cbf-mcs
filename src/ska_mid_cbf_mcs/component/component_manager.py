bbd1b08653ae25cdf72b6b35a923cbe8bcc6df0c
Owen Douglas <owen.douglas@mda.space>
2024-12-03 19:51:34 +0000

    CIP-2395 Change events for TalonBoard

--- src/ska_mid_cbf_mcs/component/component_manager.py
+++ src/ska_mid_cbf_mcs/component/component_manager.py
@@ -696,7 +696,7 @@ class CbfComponentManager(TaskExecutorComponentManager):
         self: CbfComponentManager, proxy: context.DeviceProxy
     ) -> None:
         """
-        Unsubscribe from a proxy's longRunningCommandResult attribute.
+        Unsubscribe from all of a proxy's attribute subscriptions.
 
         :param proxy: DeviceProxy
         """
@@ -704,9 +704,7 @@ class CbfComponentManager(TaskExecutorComponentManager):
         dev_name = proxy.dev_name()
         dev_events = self.event_ids.pop(dev_name, None)
         if dev_events is None:
-            self.logger.debug(
-                f"No longRunningCommandResult event subscription for {dev_name}"
-            )
+            self.logger.debug(f"No change event subscription for {dev_name}")
             return
         for attr_name, event_id in dev_events.items():
             self.logger.debug(
