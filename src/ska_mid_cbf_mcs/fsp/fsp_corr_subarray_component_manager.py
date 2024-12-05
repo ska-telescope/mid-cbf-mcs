2e0a0af42c3b6fcf72f53b6865fbdbddc179deb6
danielpoonmda <daniel.poon@mda.space>
2024-12-05 22:50:54 +0000

    CIP-2364: 32k test

--- src/ska_mid_cbf_mcs/fsp/fsp_corr_subarray_component_manager.py
+++ src/ska_mid_cbf_mcs/fsp/fsp_corr_subarray_component_manager.py
@@ -188,7 +188,7 @@ class FspCorrSubarrayComponentManager(CbfObsComponentManager):
 
         # TODO: Assume gain is an array, check with JSON Validator, consider lowering the size of the array for testing, also print on HPS side and validate
         # For now, we will reinitialize gain as a large (32k) sized array in the component manager
-        hps_fsp_configuration["fine_channelizer"]["gain"] = [1] * 16384
+        hps_fsp_configuration["fine_channelizer"]["gain"] = [1] * (16384*2)
 
         gain_corrections = GAINUtils.get_vcc_ripple_correction(
             hps_fsp_configuration["configure_scan"]["frequency_band"],
