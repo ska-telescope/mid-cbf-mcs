92c907f4550f22d007459b3f7a24652eccf66445
danielpoonmda <daniel.poon@mda.space>
2024-12-19 19:28:09 +0000

    CIP-2364: Lint again

--- src/ska_mid_cbf_tdc_mcs/fsp/fsp_corr_subarray_component_manager.py
+++ src/ska_mid_cbf_tdc_mcs/fsp/fsp_corr_subarray_component_manager.py
@@ -18,12 +18,12 @@ import tango
 from ska_control_model import CommunicationStatus, PowerState, TaskStatus
 from ska_tango_base.commands import ResultCode
 from ska_tango_testing import context
+
 from ska_mid_cbf_tdc_mcs.commons.gain_utils import GAINUtils
 from ska_mid_cbf_tdc_mcs.commons.global_enum import const, freq_band_dict
 from ska_mid_cbf_tdc_mcs.component.obs_component_manager import (
     CbfObsComponentManager,
 )
-
 
 FSP_CORR_PARAM_PATH = "mnt/fsp_param/internal_params_fsp_corr_subarray.json"
 
